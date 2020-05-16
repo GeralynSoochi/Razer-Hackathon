e7fee799f2539d8521f443820d92d39916ee1bcc
Geralyn <geralynnn@gmail.com>
2020-05-16 21:55:53 +0800

    Updates: encodedKey

--- Customer/customer.py
+++ Customer/customer.py
@@ -21,14 +21,16 @@ class Customer(db.Model):
     postalCode = db.Column(db.String(50), nullable=False)
     accountID = db.Column(db.String(50), primary_key=True)
     points = db.Column(db.Integer)
+    encodedKey = db.Column(db.String(50), nullable=False)
 
 
-    def __init__(self,accountID, username, password ,postalCode):
+    def __init__(self,accountID, username, password ,postalCode, encodedKey):
         self.accountID = accountID
         self.username = username
         self.password = password
         self.postalCode = postalCode
         self.points = 0
+        self.encodedKey = encodedKey
 
     def json(self):
         customer_entry = {
@@ -37,6 +39,7 @@ class Customer(db.Model):
             "postalcode": self.postalCode,
             "accountID": self.accountID,
             "points" : self.points
+            "encodedKey" : self.encodedKey
         }
         return customer_entry
 
@@ -53,7 +56,7 @@ def newCustomer(accountID):
         return jsonify({"message": "An accountID with '{}' already exists.".format(accountID)}), 400
 
     data = request.get_json()
-    customer = Customer(accountID, data['username'], sha256_crypt.hash(data['password']), data['postalCode'])
+    customer = Customer(accountID, data['username'], sha256_crypt.hash(data['password']), data['postalCode'], data['encodedKey'])
     try:
         db.session.add(customer)
         db.session.commit()
