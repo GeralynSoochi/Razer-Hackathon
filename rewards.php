7fd2d953660a41325892c8988b9a885f06d83d5a
Geralyn <geralynnn@gmail.com>
2020-05-17 08:53:44 +0800

    update

--- rewards.php
+++ rewards.php
@@ -242,7 +242,7 @@
         }
 
 
-        var serviceURL2 = "http://54.169.136.72:5022/redeemRewards/" + accountID + "/" + rewardID + "/" + points
+        var serviceURL2 = "http://54.169.136.72:5022/redeemRewards/" + accountID + "/" + rewardID + "/" + points;
         try {
             const response = await fetch(serviceURL2, {
                 method: 'PUT',
